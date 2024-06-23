09d47e8fb646ec921c35460e6af01e2a05194bda
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-06-23 09:07:22 +0100

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import HeroSection from "@/components/HeroSection";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="relative bg-green-400">
+      <Navbar />
       <HeroSection />
     </main>
   );
