985f620ece0c9edb5429d0a3a449a5dfb37e15cd
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-06-23 13:29:07 +0100

    changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,53 @@
+import { useEffect, useState } from "react";
+import MenuItem from "./MenuItem";
+
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const handleScroll = () => {
+    const scrollTop = window.scrollY;
+    if (scrollTop > window.innerHeight-20) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <nav className="absolute z-10 w-full p-2 py-3">
+    <nav
+      className={`fixed top-0 z-30 w-full p-2 py-3 ${
+        isScrolled ? "bg-[#313131] shadow-lg" : "bg-transparent"
+      }`}
+    >
       <div className="container mx-auto">
         <div className="flex justify-between items-center">
           <div className="">
             {" "}
-            <h1 className="text-white font-rowdies lg:text-xl text-2xl">Vibe</h1>
+            <h1 className="text-white font-rowdies font-bold lg:text-xl text-2xl">
+              Vibe
+            </h1>
           </div>
           <div className="">
             <ul className=" space-x-6 hidden md:flex">
-              <li className="text-white font-poppins cursor-pointer">Home</li>
-              <li className="text-white font-poppins cursor-pointer">About</li>
-              <li className="text-white font-poppins cursor-pointer">Contact</li>
+              <li className="text-white font-poppins cursor-pointer">
+                <MenuItem text="Home" />
+              </li>
+              <li className="text-white font-poppins cursor-pointer">
+                <MenuItem text="About" />
+              </li>
+              <li>
+                <MenuItem text="Contact" />
+              </li>
             </ul>
           </div>
           <div className="">
-            <button className="bg-white text-black lg:px-4 lg:py-2 text-sm lg:text-xl px-2 py-2 rounded-lg font-poppins">
+            <button className="bg-white text-black lg:px-4 lg:py-2 text-sm lg:text-[16px] px-2 py-2 rounded-lg font-poppins">
               Join Now
             </button>
           </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,92 @@
+import CallToAction from "@/components/CallToAction";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
+import { Metadata } from "next";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import Head from "next/head";
+import FAQ from "@/components/FAQ";
+
+export const metadata: Metadata = {
+  title: "Acme Dashboard",
+  description: "The official Next.js Course Dashboard, built with App Router.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
 
 export default function Home() {
+  const animationLayerRef = useRef<HTMLDivElement>(null);
+  const lettersRef = useRef<HTMLDivElement[]>([]);
+
+  useEffect(() => {
+    scrollTo(0,0)
+    const timeline = gsap.timeline();
+
+    // Animate each letter with a drop effect
+    timeline.fromTo(
+      lettersRef.current,
+      { y: -100, opacity: 0 },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        stagger: 0.1,
+        ease: "bounce.out",
+      }
+    );
+
+    // Create a radial reveal effect and scroll up
+    timeline.to(animationLayerRef.current, {
+      borderBottomLeftRadius: "50%",
+      borderBottomRightRadius: "50%",
+      
+      opacity: 0.9,
+      duration: 1.5,
+      ease: "power2.inOut",
+    });
+
+    // Scroll up and disappear
+    timeline.to(animationLayerRef.current, {
+      y: "-100vh",
+      duration: 2,
+      opacity: 0.8,
+      ease: "power2.in",
+      onComplete: () => {
+        if (animationLayerRef.current) {
+          animationLayerRef.current.style.display = "none";
+        }
+      },
+    });
+  }, []);
+
   return (
-    <main className="relative bg-green-400">
+    <main className="relative">
+      {/* <div
+        ref={animationLayerRef}
+        className="absolute inset-0 flex items-start justify-center bg-[#313131] z-50"
+      >
+        <div className="font-rowdies mt-20 flex space-x-2 text-white text-6xl font-bold">
+          {["V", "I", "B", "E"].map((letter, index) => (
+            <div
+              key={index}
+              ref={(el) => {
+                if (el) lettersRef.current[index] = el;
+              }}
+              className="letter"
+            >
+              {letter}
+            </div>
+          ))}
+        </div>
+      </div> */}
+      <Head>
+        <title>Vibe - Start a Chat, Catch a Vibe</title>
+        <meta name="description" content="Vibe - Start a Chat, Catch a Vibe" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Navbar />
       <HeroSection />
+      <CallToAction />
+      <FAQ/>
     </main>
   );
 }
