3c0738a2956f1aa66b54cd3fd776161404428537
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-06-23 15:59:43 +0100

    Added a side nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,18 @@
+import { IoMdMenu, IoMdClose } from "react-icons/io";
 import { useEffect, useState } from "react";
 import MenuItem from "./MenuItem";
 import Link from "next/link";
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [showSideMennu, setSHowSideMenu] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const handleScroll = () => {
     const scrollTop = window.scrollY;
@@ -14,25 +23,63 @@ const Navbar = () => {
     }
   };
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+  const showSideMenu = () => {
+    setSHowSideMenu(!showSideMennu);
+  };
+
   return (
     <nav
-      className={`fixed top-0 z-30 w-full p-2 py-3 ${
+      className={`fixed top-0 z-30 w-full ${
         isScrolled ? "bg-[#313131] shadow-lg" : "bg-transparent"
       }`}
     >
-      <div className="container mx-auto">
-        <div className="flex justify-between items-center">
+      {showSideMennu && (
+        <div className="absolute h-screen bg-[#444444] text-white w-[75%] z-50 border-r-[1px] border-r-[#353535] p-5 flex flex-col justify-between">
+          <div className="flex justify-between items-center">
+            <div className="">
+              <h1 className="text-2xl -mb-2 font-rowdies font-extrabold">
+                Vibe
+              </h1>
+              <small className="font-poppins">
+                Start a chat, catch a vibe...
+              </small>
+            </div>
+            <button onClick={() => showSideMenu()}>
+              <IoMdClose color="red" size={30} />
+            </button>
+          </div>
           <div className="">
+            <ul className="flex flex-col">
+              <li className="border-b-[1px] border-b-[#313131] py-3">
+                <Link href="/#home">
+                  <MenuItem text="Home" bold={true}/>
+                </Link>
+              </li>
+              <li className="border-b-[1px] border-b-[#313131] py-3">
+                <Link href="/#features">
+                  <MenuItem text="About" bold={true}/>
+                </Link>
+              </li>
+              <li className="py-3">
+                <Link href="/#contact">
+                  <MenuItem text="Contact" bold={true}/>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <small className="font-rowdies">&copy; Vibe 2024.</small>
+          </div>
+        </div>
+      )}
+      <div className="container mx-auto p-2 py-3">
+        <div className="flex justify-between items-center">
+          <div className="flex gap-2 items-center font-rowdies">
             {" "}
-            <h1 className="text-white font-rowdies font-bold lg:text-xl text-2xl">
-              Vibe
-            </h1>
+            <button className="block md:hidden" onClick={() => showSideMenu()}>
+              <IoMdMenu color="white" size={25} />
+            </button>
+            <h1 className="text-white  font-bold lg:text-xl text-2xl">Vibe</h1>
           </div>
           <div className="">
             <ul className=" space-x-6 hidden md:flex">
